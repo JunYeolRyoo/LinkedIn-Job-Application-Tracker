04993011f5ad774a0a9ed9852fb150c014cbc25a
JunYeolRyoo <ryoojunyeol@gmail.com>
2024-07-18 21:55:27 -0500

    Made some changes

--- new.py
+++ new.py
@@ -11,49 +11,53 @@ import time
 chrome_options = Options()
 chrome_options.add_argument("--window-size=1920x1080")  # Set the window size
 
-
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()) , options=chrome_options)
 
-driver.get("https://www.linkedin.com/my-items/saved-jobs/")
+url = "https://www.linkedin.com/my-items/saved-jobs/"
+driver.get(url)
 
 dic = {}
 
 buttonClicked = False
 addedElem = 0
 first = True
-while True:
-    try:
-        elements = driver.find_elements(By.CSS_SELECTOR,"ul.reusable-search__entity-result-list li.reusable-search__result-container")
-        for i in elements:
-            try:
-                companyName= i.find_element(By.CSS_SELECTOR,"div.entity-result__primary-subtitle.t-14.t-black.t-normal")
-                role_element = i.find_element(By.CSS_SELECTOR, "span.entity-result__title-text.t-16 a.app-aware-link")
 
-                if companyName.text not in dic:
-                    dic[companyName.text] = [role_element.text]
-                else:
-                    dic[companyName.text].append(role_element.text)
-                addedElem += 1
-            except:
-                print("here")
+while True:
+    if url in driver.current_url:   # Check if the current url is indeed "https://www.linkedin.com/my-items/saved-jobs/..."
         try:
-            if first:
-                time.sleep(7)
+            if first:   # Implicitly wait at the first time to get list of applied jobs
+                driver.implicitly_wait(10)
                 first = False
-            button = WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located((By.CSS_SELECTOR, 'button.artdeco-pagination__button--next'))
-            )
-            if button.is_enabled():
-                driver.execute_script("arguments[0].click();", button)
-                print("button clicked")
-                time.sleep(3)
-            else:
-                break
-        except:
-            print("no next button available")
-            
-    except NoSuchElementException:
-        print("no such element")
-print(dic)
+
+            # Get all applied jobs showing on the current page (10 applied jobs per page)
+            elements = driver.find_elements(By.CSS_SELECTOR,"ul.reusable-search__entity-result-list li.reusable-search__result-container")
+
+            for i in elements:
+                try:
+                    companyName= i.find_element(By.CSS_SELECTOR,"div.entity-result__primary-subtitle.t-14.t-black.t-normal")
+                    role_element = i.find_element(By.CSS_SELECTOR, "span.entity-result__title-text.t-16 a.app-aware-link")
+
+                    if companyName.text not in dic:
+                        dic[companyName.text] = [role_element.text]
+                    else:
+                        dic[companyName.text].append(role_element.text)
+                    addedElem += 1
+                except Exception as e:
+                    print("Error: ", e)
+            try:
+                button = WebDriverWait(driver, 10).until(
+                    EC.presence_of_element_located((By.CSS_SELECTOR, 'button.artdeco-pagination__button--next'))
+                )
+                if button.is_enabled():
+                    driver.execute_script("arguments[0].click();", button)
+                    # time.sleep(3)
+                else:
+                    break   # If the "Next" button is no more available, break the loop
+            except Exception as e:
+                print("Error: ", e)
+                
+        except Exception as e:
+            print("Error: ", e)
+
 print(addedElem)
 driver.quit()
